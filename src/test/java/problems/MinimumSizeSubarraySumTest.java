a1b417a865ed54c99d92f661981a7b7ea301798e
@gosek <44835811+michalgosek@users.noreply.github.com>
2024-02-25 21:08:31 +0100

    Added solution for minimum-size-subarray-sum leetcode problem. (#4)

---
+++ src/test/java/problems/MinimumSizeSubarraySumTest.java
@@ -0,0 +1,31 @@
+package problems;
+
+import com.michalgosek.problems.MinimumSizeSubarraySum;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class MinimumSizeSubarraySumTest {
+    static Stream<Arguments> minimumSizeSubarraySumTestData() {
+        return Stream.of(
+                arguments(new int[]{2, 3, 1, 2, 4, 3}, 7, 2),
+                arguments(new int[]{1, 4, 4}, 4, 1),
+                arguments(new int[]{1, 1, 1, 1, 1, 1, 1, 1}, 11, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("minimumSizeSubarraySumTestData")
+    public void shouldReturnIndexAccordingToTestData(int[] nums, int target, int expectedSubarraySize) {
+        // when:
+        var minimumSubarraySize = MinimumSizeSubarraySum.Solution(target, nums);
+
+        // then:
+        assertEquals(expectedSubarraySize, minimumSubarraySize);
+    }
+}
