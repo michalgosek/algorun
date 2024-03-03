0b9f837004f91a89778fcbe1f97cf2fdad446b78
Michal Gosek <inbox@michalgosek.com>
2024-03-03 14:09:36 +0100

    Added example problems solution for prefix sums.

---
+++ src/test/java/algorithms/twopointers/SubarraySumTest.java
@@ -0,0 +1,34 @@
+package algorithms.twopointers;
+
+import com.michalgosek.problems.SubarraySum;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class SubarraySumTest {
+    static Stream<Arguments> SubarraySumTestData() {
+        return Stream.of(
+                arguments(Arrays.asList(1, 3, -3, 8, 5, 7), 1, List.of(0, 1)),
+                arguments(Arrays.asList(1, 3, -3, 8, 5, 7), 5, List.of(2, 4)),
+                arguments(Arrays.asList(1, 3, -3, 8, 5, 7), 12, List.of(4, 6)),
+                arguments(Arrays.asList(1, 3, -3, 8, 5, 7), 4, List.of(0, 2))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("SubarraySumTestData")
+    public void shouldReturnSubarrayIndicesAccordingToTestData(List<Integer> numbers, int target, List<Integer> expectedIndices) {
+        // act:
+        var subarrayIndices = SubarraySum.subarraySum(numbers, target);
+
+        // arrange:
+        assertEquals(expectedIndices, subarrayIndices);
+    }
+}
---
+++ src/test/java/algorithms/twopointers/SubarraySumTotalTest.java
@@ -0,0 +1,34 @@
+package algorithms.twopointers;
+
+import com.michalgosek.problems.SubarraySumTotal;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class SubarraySumTotalTest {
+    static Stream<Arguments> subarraySumTotalTestData() {
+        return Stream.of(
+                arguments(Arrays.asList(1, 4, 1, 4), 5, 3),
+                arguments(Arrays.asList(1, 4, 1, 4), 1, 2),
+                arguments(Arrays.asList(1, 4, 1, 4, 1, 4), 6, 2),
+                arguments(Arrays.asList(10, 5, -5, -20, 10), -10, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("subarraySumTotalTestData")
+    public void shouldReturnTotalSubarrayCountEqualsToTargetAccordingToTestData(List<Integer> numbers, int target, int expectedSubarrayNumbers) {
+        // act:
+        var totalSubarrayCount = SubarraySumTotal.subarraySumTotal(numbers, target);
+
+        // arrange:
+        assertEquals(expectedSubarrayNumbers, totalSubarrayCount);
+    }
+}
