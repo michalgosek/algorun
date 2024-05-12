2cbbbc7b02ec744ed0148fa43fff249af9c7c798
Michal Gosek <inbox@michalgosek.com>
2024-05-12 14:16:29 +0200

    Added two element array for sorting test data.

--- src/test/java/algorithms/sorting/SortingTestDataSource.java
+++ src/test/java/algorithms/sorting/SortingTestDataSource.java
@@ -10,7 +10,8 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class SortingTestDataSource {
     public static Stream<Arguments> DataSet() {
         return Stream.of(
-                arguments(Arrays.asList(3, 4, 2, -1, 1, 0), Arrays.asList(-1, 0, 1, 2, 3, 4))
+                arguments(Arrays.asList(3, 4, 2, -1, 1, 0), Arrays.asList(-1, 0, 1, 2, 3, 4)),
+                arguments(Arrays.asList(1, 0), Arrays.asList(0, 1))
         );
     }
 }
